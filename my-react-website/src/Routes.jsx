c56d3e7bb3d2d5bc440935dfe2553f67d700f15b
Theotim Chevalier <theotimc.chevalier@free.fr>
2025-10-07 14:38:36 +0200

    bug fixe

--- my-react-website/src/Routes.jsx
+++ my-react-website/src/Routes.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles/App.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from '/pages/HomePage/HomePage';
+import App from './App.jsx';
+import HomePage from './pages/HomePage/HomePage';
 
 const router = createBrowserRouter([
   {
+    path: '/',
     element: <App />,
     children: [
       {
-        path: '/home',
-        element: <HomePage/>,
+        path: 'home',
+        element: <HomePage />,
       },
-
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+export default function Routes() {
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
